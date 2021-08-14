f267ee61fb259ece47a17068af55df9f401592fe
hasithsen <it17142038@my.sliit.lk>
2021-08-14 21:26:32 +0530

    Fix staticfiles path error

--- sayit/settings/base.py
+++ sayit/settings/base.py
@@ -104,11 +104,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-# The absolute path to the directory where collecstatic will collect static files for deployment
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# The absolute path to the directory where collecstatic will collect static files for deployment
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 HASHIDS_SALT = env('HASHIDS_SALT')
