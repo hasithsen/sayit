3bcc03c52d1c440b36782b48bf58deb531bb5908
hasithsen <it17142038@my.sliit.lk>
2021-08-14 18:07:50 +0000

    Fix message link copy function

--- stage/static/js/main.js
+++ stage/static/js/main.js
@@ -1,10 +1,4 @@
 function copyMessageDetailURL() {
-  var textToCopy = document.getElementById('id_message-detail-url');
-
-  textToCopy.select();
-  textToCopy.setSelectionRange(0, 99999); /* For mobile devices */
-
-  document.execCommand('copy');
-
-  alert('Copied to clipboard!');
+  var textToCopy = document.getElementById('id_message-detail-url').value;
+  window.prompt("Copy to clipboard: Ctrl+C, Enter", textToCopy);
 }
